524c782f67c7c9d6cdb5e59da9b527721f12a920
Huzaifa785 <huzaifa.coder785@gmail.com>
2023-08-12 00:06:03 +0530

    minor update

--- api/app.py
+++ api/app.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for
 from bs4 import BeautifulSoup
 import requests
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 
-@application.route("/")
+@app.route("/")
 def index():
     return render_template("index.html")
 
 
-@application.route("/review", methods=["GET", "POST"])
+@app.route("/review", methods=["GET", "POST"])
 def review():
     if request.method == "POST":
         product_url = request.form["product_url"]
@@ -70,4 +70,4 @@ def review():
 
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    app.run(debug=True)
